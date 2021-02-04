b26e73c11c2daf3a3fee2512f3b3a590fca1ed8f
David Ballantyne <david.hk.ballantyne@gmail.com>
2021-02-04 09:31:05 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+echo $HOSTNAME
 echo Backing up
 FOLDER=/mnt/gdrive/plexguide/backup
 DOCKERS=`ls /opt/appdata/`
 echo $FOLDER
 DATES=`date +%Y-%m-%d_%H.%M`
 echo $DATES
-rclone --config=/opt/appdata/uploader/rclone.conf mkdir gdrive:/plexguide/backup/archive/$DATES -v --stats 2s
+rclone --config=/opt/appdata/uploader/rclone.conf mkdir gdrive:/plexguide/backup/archive/$HOSTNAME/$DATES -v --stats 2s
 echo ARCHIVING OLD BACKUPS
-rclone --config=/opt/appdata/uploader/rclone.conf -v --stats 5s copy gdrive:/plexguide/backup/gce-master/ gdrive:/plexguide/backup/archive/$DATES/
+rclone --config=/opt/appdata/uploader/rclone.conf -v --stats 5s copy gdrive:/plexguide/backup/$HOSTNAME/ gdrive:/plexguide/backup/archive/$HOSTNAME/$DATES/
 
 for VAR in $DOCKERS
 do
@@ -16,3 +17,4 @@ do
         echo $VAR >/tmp/program_var && bash /opt/pgvault/pgcron
 
 done
+echo Testing complete $date
