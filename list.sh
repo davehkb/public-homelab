47b7bbfd5e80aa4bf37f420e196b477f45322443
David Ballantyne <david.hk.ballantyne@gmail.com>
2021-05-18 11:36:29 -0400

    Update list.sh

--- list.sh
+++ list.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 echo hello
 TRAK="docker exec -i traktarr /traktarr/traktarr.py"
-echo $TRAK
-#docker exec -i traktarr /traktarr/traktarr.py movies -t trending
+LIST="trending anticipated popular played"
+M=50
+S=5
+
+for x in $LIST
+do
+        echo ""
+        echo $x
+        if [ $x = trending ] || [ $x = anticipated ]
+        then
+                echo $x is trending
+                $TRAK movies -t $x
+                echo sleep for 2s
+                sleep 30s
+        else
+                $TRAK movies -t $x -l $M
+                echo sleep for 2S
+                sleep 30s
+        fi
+done
+
+
+for x in $LIST
+do
+        echo ""
+        echo $x
+        if [ $x = anticipated ]
+        then
+                echo $x is trending
+                $TRAK shows -t $x
+                echo sleep for 2s
+                sleep 30s
+        else
+                $TRAK shows -t $x -l $S
+                echo sleep for 2S
+                sleep 30s
+        fi
+done
