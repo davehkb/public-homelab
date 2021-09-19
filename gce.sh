d99e144d79a0166bd6928d68157c583eddecf63e
David Ballantyne <david.hk.ballantyne@gmail.com>
2021-09-19 10:13:11 -0400

    Update gce.sh

--- gce.sh
+++ gce.sh
@@ -8,3 +8,12 @@ sleep 2s
 $s
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAhydYLT6JTiIdxQw5nJlkyTwgwIwWLeu7dpz4+Xnvv24tEFIDkthTFgVnSbXe1NzP9VsosSmQr22qByTQ3NLCgKeZYNCSS+LfDksSl1twTFNFr5u2J8o/B+sGyA5YuFIoBwJw/m580C99zhjn75S+ndsthlWCIsdPd3UBXU1l48t2iH7JUTMXdnUNQtQnga2L+WFhW6YuIGGGgEwuCd/fqi8K0SQ3k6fVE/dtf1A5ivSKuUk2qPdnaCcMQa2OlrR6+OvBVouSqgO/nGkDiE5bXkflBon8c71sf6/h3Mk7UbNG2T/hkLM1vGRg+gkQ3W4JUdRoNx8ZbQoS4TO4fJMF1Q== rsa-key-20200602" >> /root/.ssh/authorized_keys
 service sshd restart
+sudo apt-get update -yqq
+sudo apt-get upgrade -yqq
+sudo apt-get autoclean -yqq
+sudo apt-get install wget -y
+sleep 2s
+curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
+sudo bash add-google-cloud-ops-agent-repo.sh --also-install
+sleep 2s
+sudo wget -qO- https://raw.githubusercontent.com/MHA-Team/Install/master/install.sh | sudo bash
